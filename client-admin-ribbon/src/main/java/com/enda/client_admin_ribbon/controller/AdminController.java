b8156c597d5bf6d22d236ff32c41bd8aed07b0b4
CHINAINMYHEART <1139914806@qq.com>
2019-04-10 09:47:51 +0800

    ribbon引入熔断器

--- client-admin-ribbon/src/main/java/com/enda/client_admin_ribbon/controller/AdminController.java
+++ client-admin-ribbon/src/main/java/com/enda/client_admin_ribbon/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.enda.client_admin_ribbon.controller;
 import com.enda.client_admin_ribbon.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @EnableDiscoveryClient
+@EnableHystrix
 public class AdminController {
 
     @Autowired
--- client-admin-ribbon/src/main/java/com/enda/client_admin_ribbon/service/AdminService.java
+++ client-admin-ribbon/src/main/java/com/enda/client_admin_ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.enda.client_admin_ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,15 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * 在 Ribbon 调用方法上增加 @HystrixCommand 注解并指定 fallbackMethod 熔断方法
+     */
+    @HystrixCommand(fallbackMethod = "sayHelloError")
     public String sayHello(String name) {
         return restTemplate.getForObject("http://service-admin/admin?name=" + name, String.class);
+    }
+
+    public String sayHelloError(String name) {
+        return String.format("Your message is %s, but request error.", name);
     }
 }
